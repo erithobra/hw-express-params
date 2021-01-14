0c45c0846bf7eb9d313c09b209c15298ce7578eb
ericb2020 <ebrauer83@gmail.com>
2021-01-14 16:26:40 -0600

    Commit 2: Tip Calculator express application created.

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.get("/tip/:total/:tipPercentage", (req,res) => {
+    let x = [req.params.total] * ([req.params.tipPercentage] / 100);
+    let y = x.toString(10);
+    res.send(y);
+});
 
 app.get("/greeting/:name", (req, res) => {
     res.send(`Hi there, ${req.params.name}`)
 
 })
-
 
 app.get("/greeting/", (req,res) => {
     res.send("Hello, stranger.");
