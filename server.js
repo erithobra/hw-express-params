f7cd45675f0e9f3166ec240c800f2e5e019b8cc9
ericb2020 <ebrauer83@gmail.com>
2021-01-14 17:07:07 -0600

    Commit 3: Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+const magicArray = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+
+function answer() {
+    let x = Math.floor(Math.random() * magicArray.length);
+    return magicArray[x];
+}
+
+app.get("/magic/:question", (req,res) => {
+
+    res.send(`${req.params.question} <br> ${answer()}`);
+})
+
 app.get("/tip/:total/:tipPercentage", (req,res) => {
     let x = [req.params.total] * ([req.params.tipPercentage] / 100);
     let y = x.toString(10);
